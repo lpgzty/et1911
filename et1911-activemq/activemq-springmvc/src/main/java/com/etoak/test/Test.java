47cb7d4a97bb5de6df5069cad4eb880f8f1f039e
et1911 <et1911@et.com>
2021-04-09 09:33:13 +0800

    tianjia

---
+++ et1911-activemq/activemq-springmvc/src/main/java/com/etoak/test/Test.java
@@ -0,0 +1,12 @@
+package com.etoak.test;
+
+/**
+ * @Description: TODO
+ * @author: scott
+ * @date: 2021-04-09$ 9:31$
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("HELLO,WORLD!");
+    }
+}
